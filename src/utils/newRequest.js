8c644b593f22cd6d0792dc1ed48eb0b88572e052
chardoo <40030004+chardoo@users.noreply.github.com>
2023-04-17 18:28:31 +0000

    refactoring

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -4,7 +4,7 @@ const tokenExists = false; // work on this later rich code
 const authToken = ""; // from cookies - whatever 
 
 const newRequest = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
+  baseURL: "https://z5anzvah2w.us-east-1.awsapprunner.com",
   withCredentials: false,
   headers: {
     Accept: "Application/json",
